2ef99f66bb6e9110c7258b815521795802eb47dd
Robin Novak <robin.m.novak@gmail.com>
2020-12-03 11:22:09 -0800

    Swipe and Delete Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,6 +9,7 @@ import {createDrawerNavigator, DrawerItems} from 'react-navigation-drawer';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 import {Icon} from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
 import {connect} from 'react-redux';
@@ -143,6 +144,29 @@ const ReservationNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: {screen: Favorites},
+    },
+    {
+        defaultNavigationOptions: ({navigation})=> ({
+            headerStyle:{
+                backgroundColor: '#5637DD',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                    name = 'heart'
+                    type = 'font-awesome'
+                    iconStyle = {styles.stackIcon}
+                    onPress = {() => navigation.toggleDrawer()}
+                />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView
@@ -200,6 +224,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name= 'tree'
+                        type = 'font-awesome'
+                        size = {24}
+                        color = {tintColor}
+                    />
+                )
+            }
+        },
+        Favorites: {
+            screen : FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name= 'heart'
                         type = 'font-awesome'
                         size = {24}
                         color = {tintColor}
